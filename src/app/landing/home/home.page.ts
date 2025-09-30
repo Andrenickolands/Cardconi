be97c774149b8478fe044f541edfd1fbd2cff7f8
Andrenickolands <ing.nicolassepulveda@egmail.com>
2025-09-30 13:40:53 -0500

    home

--- src/app/landing/home/home.page.ts
+++ src/app/landing/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent } from '@ionic/angular/standalone';
+import { IonContent, IonButton } from '@ionic/angular/standalone';
 import { HeaderLandingPage } from "src/app/components/header/header-landing/header-landing.page";
 import { BtnCtaPage } from "src/app/components/btns/btn-cta/btn-cta.page";
 import { FooterLandingPage } from "src/app/components/footer/footer-landing/footer-landing.page";
@@ -11,7 +11,7 @@ import { FooterLandingPage } from "src/app/components/footer/footer-landing/foot
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule, HeaderLandingPage, BtnCtaPage, FooterLandingPage]
+  imports: [IonButton, IonContent, CommonModule, FormsModule, HeaderLandingPage, BtnCtaPage, FooterLandingPage]
 })
 export class HomePage implements OnInit {
 
