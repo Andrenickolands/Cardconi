dbf98075176e027b9d90df2f08259bf73eeabdc6
DAvid <david.vt.sb@gmail.com>
2025-10-02 00:19:13 -0500

    animation line

--- src/app/landing/home/home.page.ts
+++ src/app/landing/home/home.page.ts
@@ -15,10 +15,18 @@ import { BtnCartPage } from "src/app/components/btns/btn-cart/btn-cart.page";
   imports: [IonContent, CommonModule, FormsModule, HeaderLandingPage, BtnCtaPage, FooterLandingPage, BtnCartPage]
 })
 export class HomePage implements OnInit {
+  scrollProgress: number = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onContentScroll(event: any) {
+    const scrollTop = event.detail.scrollTop;
+    const scrollHeight = event.detail.scrollHeight - event.detail.clientHeight;
+    
+    this.scrollProgress = Math.min(scrollTop / Math.max(scrollHeight, 1), 1);
   }
 
 }
