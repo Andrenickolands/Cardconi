c2127a04ab9ccb9b725037fa8e920fc880d29899
Andrenickolands <ing.nicolassepulveda@egmail.com>
2025-10-02 16:37:13 -0500

    home

--- src/app/landing/home/home.page.ts
+++ src/app/landing/home/home.page.ts
@@ -6,16 +6,18 @@ import { HeaderLandingPage } from "src/app/components/header/header-landing/head
 import { BtnCtaPage } from "src/app/components/btns/btn-cta/btn-cta.page";
 import { FooterLandingPage } from "src/app/components/footer/footer-landing/footer-landing.page";
 import { BtnCartPage } from "src/app/components/btns/btn-cart/btn-cart.page";
+import { ReviewCardPage } from "src/app/components/cards/review-card/review-card.page";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule, HeaderLandingPage, BtnCtaPage, FooterLandingPage, BtnCartPage, IonAccordion]
+  imports: [IonContent, CommonModule, FormsModule, HeaderLandingPage, BtnCtaPage, FooterLandingPage, BtnCartPage, IonAccordion, ReviewCardPage]
 })
 export class HomePage implements OnInit {
   scrollProgress: number = 0;
+  cardsHovered: boolean[] = [false, false];
 
   constructor() { }
 
@@ -25,8 +27,11 @@ export class HomePage implements OnInit {
   onContentScroll(event: any) {
     const scrollTop = event.detail.scrollTop;
     const scrollHeight = event.detail.scrollHeight - event.detail.clientHeight;
-    
     this.scrollProgress = Math.min(scrollTop / Math.max(scrollHeight, 1), 1);
+  }
+
+  hoverCard(index: number, isHovering: boolean) {
+    this.cardsHovered[index] = isHovering;
   }
 
 }
