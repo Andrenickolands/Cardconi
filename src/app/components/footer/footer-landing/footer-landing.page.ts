8711fb11ff8ffd8f6c92e85f5105904f144f6aef
DAvid <david.vt.sb@gmail.com>
2025-10-03 10:07:43 -0500

    update

--- src/app/components/footer/footer-landing/footer-landing.page.ts
+++ src/app/components/footer/footer-landing/footer-landing.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonFooter } from '@ionic/angular/standalone';
+import { IonFooter, IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-footer-landing',
   templateUrl: './footer-landing.page.html',
   styleUrls: ['./footer-landing.page.scss'],
   standalone: true,
-  imports: [IonFooter, CommonModule, FormsModule]
+  imports: [IonIcon, IonFooter, CommonModule, FormsModule]
 })
 export class FooterLandingPage implements OnInit {
 
